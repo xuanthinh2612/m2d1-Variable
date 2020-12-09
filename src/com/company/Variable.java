88cecb2b4d04f43476e6f40c93dcfab8db67299d
gio lang du <you@example.com>
2020-12-09 16:33:29 +0700

    variable

---
+++ src/com/company/Variable.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Variable {
+
+    public static void main(String[] args) {
+        // write your code here
+        int i = 10;
+        float f = 20.5f;
+        double d = 20.5d;
+        boolean b = true;
+        char c = 'a';
+        String s = "Hà Nội";
+        System.out.println("i = " + i);
+        System.out.println("f = " + f);
+        System.out.println("d = " + d);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("s = " + s);
+    }
+}
